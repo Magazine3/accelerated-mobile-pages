821b2cd83099f1376d2e67b6f544c19204b361b3
Raju Jeelaga <rajujeelaga@gmail.com>
2018-01-30 11:19:48 +0530

    category and post date is added and styling is done
    category and post date is added and styling is done

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,9 +10,23 @@
 ?>
 	<div class="fsp">
 		<div class="fsp-img">
-			<?php amp_wordpress_theme_post_thumbnail('module-1'); ?>
+			<?php the_post_thumbnail('module-1'); ?>
 		</div>
 		<div class="fsp-cnt">
+			<div class="category-lists">
+               <?php 
+                  $id = get_the_ID();
+                  $cats = get_the_category($id);
+                  echo ( count($cats) == 1  ? '' : '');
+                  $c = 0; $n = 0;
+                  $c = count($cats);
+                  foreach ( $cats as $cat ):
+                      $n++; ?>
+                      <li><a href="<?php echo get_category_link($cat->cat_ID); ?>">
+                          <?php echo $cat->name; echo ( $n > 0 && $n < $c ? '' : ''); ?>
+                      </a></li>
+                  <?php endforeach; ?>
+            </div><!-- /.category-lists -->
 			<?php
 				if ( is_singular() ) :
 					the_title( '<h1 class="entry-title">', '</h1>' );
@@ -21,5 +35,6 @@
 				endif; 
 			?>
 			<?php the_excerpt(); ?>
+			<span class="posted-dt"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?></span>
 		</div>
 	</div>
